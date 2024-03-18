1c015fe4014d236758d5977d0e134d0cb7f60078
IgBovt <140014814+IgBovt@users.noreply.github.com>
2024-03-19 00:05:42 +0200

    finished

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
 import ReactDOM from 'react-dom/client';
 import 'modern-normalize';
 import App from './components/App/App';
@@ -9,7 +10,9 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
